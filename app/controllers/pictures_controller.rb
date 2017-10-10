5dd595396a016de7d480ce78e3010b2fd89d00ae
Richard-Howard-Toronto <rh_gk@rogers.com>
2017-10-10 17:17:32 -0400

    adds edit functionality with changes i controller, edit.html.erb and show.html.erb

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -12,6 +12,24 @@ class PicturesController < ApplicationController
     @picture = Picture.new
   end
 
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+
+    @picture.title = params[:picture][:title]
+    @picture.artist = params[:picture][:artist]
+    @picture.url = params[:picture][:url]
+
+    if @picture.save
+      redirect_to "/pictures/#{@picture.id}"
+    else
+      render :edit
+    end
+  end
+
   def create
     @picture = Picture.new
 
